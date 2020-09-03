db67bca6fb65409c8623fe21da414e6f8f93179f
Noah <mike1124@naver.com>
2020-09-03 16:12:00 +0900

    Improve JS slider with Click event

--- index.js
+++ index.js
@@ -3,30 +3,52 @@
 function init() {
     const mobileMenu = document.querySelector("div.m-menu");
     const toggleBtn = document.querySelector("#toggle-btn");
-
+    // 햄버거 토글 버튼 가져옴 클릭 다시 클릭시 /resize 될때마다 메뉴바 사라짐 구현
     toggleBtn.addEventListener("click", toggleElements);
     window.addEventListener("resize", removeMobileMenu);
 
     const bg_list = [1, 2, 3]
+    let bgNumber;
     const bgContainer = document.querySelector(".bg-container");
     const bgChangeInterval = setInterval(handleChangeBg, 3000);
     const renderBgNumber = document.querySelector("#bg_num");
-    // const bgButtonContainer = document.querySelector(".bg_btn_container");
-
+    const bgButtonContainerRight = document.querySelector(".bg_button.right");
+    const bgButtonContainerLeft = document.querySelector(".bg_button.left");
     window.addEventListener("load", handleChangeBg);
     bgContainer.addEventListener("click", stopChangeBg);
-    // bgButtonContainer.addEventListener("mouseover", handleChangeBgButton)
-
+    bgButtonContainerRight.addEventListener("click", handleChangeBgButtonR);
+    bgButtonContainerLeft.addEventListener("click", handleChangeBgButtonL);
+    // 3초마다 background 변경되도록 설정 만약 background 위에 클릭 이벤트 발생시 clearInterval로 변경 제거
     function stopChangeBg() {
         clearInterval(bgChangeInterval);
     }
-
+    // background 3가지 순서대로 보여줌 화면 아래 bgNumber도 같이 변경
     function handleChangeBg() {
-        const bgNumber = bg_list.shift()
+        bgNumber = bg_list.shift()
         bgContainer.style.backgroundImage = `url("/images/background/${bgNumber}.jpg")`;
         renderBgNumber.textContent = `0${bgNumber}`;
         bg_list.push(bgNumber);
     }
+
+    function handleChangeBgButtonR() {
+        bgNumber -= 1;
+        if (bgNumber === 0) {
+            bgNumber = 3;
+        }
+        renderBgNumber.textContent = `0${bgNumber}`;
+        bgContainer.style.backgroundImage = `url("/images/background/${bgNumber}.jpg")`;
+    }
+
+    function handleChangeBgButtonL() {
+        bgNumber += 1;
+        if (bgNumber === 4) {
+            bgNumber = 1;
+        }
+
+        renderBgNumber.textContent = `0${bgNumber}`;
+        bgContainer.style.backgroundImage = `url("/images/background/${bgNumber}.jpg")`;
+    }
+
 
     // function handleChangeBgButton() {
     //     if (bgButtonContainer === null) {
@@ -39,10 +61,11 @@ function init() {
     //     bg_list.push(bgNumber);
     // }
 
+    //resize 될때 메뉴 바 사라지도록 구현
     function removeMobileMenu() {
         mobileMenu.classList.remove("on");
     }
-
+    // 토클 메서드 사용하여 클릭 / 재클릭시 "on" class 생성 제거 
     function toggleElements() {
         mobileMenu.classList.toggle('on');
     }
